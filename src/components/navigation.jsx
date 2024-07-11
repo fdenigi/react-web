50f2219d92af5da78b00d7985350e7ff88f8c54f
fdenigi <farhan.denigi@gmail.com>
2024-07-11 16:21:30 +0700

    new commit

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -18,7 +18,7 @@ export const Navigation = (props) => {
             <span className="icon-bar"></span>{" "}
           </button>
           <a className="navbar-brand page-scroll" href="#page-top">
-            React Landing Page
+            <img src="/img/C(1)(1).jpg"></img>
           </a>{" "}
         </div>
 
@@ -27,39 +27,39 @@ export const Navigation = (props) => {
           id="bs-example-navbar-collapse-1"
         >
           <ul className="nav navbar-nav navbar-right">
-            <li>
+            {/* <li>
               <a href="#features" className="page-scroll">
                 Features
               </a>
-            </li>
+            </li> */}
             <li>
               <a href="#about" className="page-scroll">
-                About
+                Tentang Kami
               </a>
             </li>
             <li>
               <a href="#services" className="page-scroll">
-                Services
+                Layanan
               </a>
             </li>
             <li>
               <a href="#portfolio" className="page-scroll">
-                Gallery
+                Portofolio
               </a>
             </li>
-            <li>
+            {/* <li>
               <a href="#testimonials" className="page-scroll">
                 Testimonials
               </a>
-            </li>
+            </li> */}
             <li>
               <a href="#team" className="page-scroll">
-                Team
+                Tim
               </a>
             </li>
             <li>
               <a href="#contact" className="page-scroll">
-                Contact
+                Kontak
               </a>
             </li>
           </ul>
